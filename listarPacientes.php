91c854cc4847ad12827c0bb8a03bd49d4ed7f5ff
Joycevânia Salvino <joycelima773@gmail.com>
2021-12-13 23:03:44 -0300

    Estilização
    ...Da página de listar e cadastrar pacientes. Foi add um svg à esquerda do nome exibido na página. Estilizar cor e posicionamento dele. E na página logout diminui o tamanho do botão.

--- listarPacientes.php
+++ listarPacientes.php
@@ -60,6 +60,9 @@
                         <a style="font-size: 15px; color:rgb(158, 156, 156);" class="nav-link" href="./logout.html">Sair</a>
                     </li>
                 </ul>
+                <svg style="color: white; margin-right: 2px; margin-top: 1px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                </svg>
                 <p style="margin-bottom: -2px;" >Olá, <?=$_SESSION['nome']?>!</p>
             </div>
         </nav>
--- pacienteCadastro.php
+++ pacienteCadastro.php
@@ -76,6 +76,10 @@
                         <a style="font-size: 15px; color:rgb(158, 156, 156);" class="nav-link" href="./logout.html">Sair</a>
                     </li>
                 </ul>
+                <svg style="color: white; margin-right: 2px; margin-top: -1px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                </svg>
+                <p style="margin-bottom: 2px; color: #ffffff;" >Olá, <?=$_SESSION['nome']?>!</p>
             </div>
         </nav>
     </header>
@@ -91,7 +95,7 @@
                 <form action="" method="POST">
                 <?php 
                 if(isset($_SESSION['error'])){?>
-                    <p style= "margin: 0; color: red;"><?= $_SESSION['error'];?></p><?php
+                    <p class="p" style= "margin: 0; color: red;"><?= $_SESSION['error'];?></p><?php
                 }else{ 
                     echo "";
                     }?>
